2f7f2a308e4506e1b12468be0b1a63015d0b7191
6704062611221 <s6704062611221@email.kmutnb.ac.th>
2025-12-16 14:10:03 +0700

    bonus1+2

--- case6a.cpp
+++ case6a.cpp
@@ -2,19 +2,35 @@
 int main(){
 	int score;
 	scanf("%d",&score);
-	if(score >= 85){
-		printf("A");
-	}
-	else if(score >= 84){
-		printf("B");
-	}
-	else if(score >= 74){
-		printf("C");
-	}
-	else if (score >= 40){
-		printf("D");
-	}
-	else if(score < 40){
-		printf("f");
+	if(score <= 100 ){
+		if(score >= 0){
+			if(score >= 85){
+				printf("A\n");
+				score = 85 - score;
+				printf("A(%d)",score);
+				}
+			else if(score >= 75){
+				printf("B\n");
+				score = 85 - score;
+				printf("A (%d)",score);
+				}
+			else if(score >= 68){
+				printf("C\n");
+				score = 75 - score;
+				printf("B(%d)",score);
+				}
+			else if (score >= 55){
+				printf("D\n");
+				score = 68-score;
+				printf("C(%d)",score);
+				}
+			else if(score < 55){
+				printf("F\n");
+				}
+		}else{
+			printf("Error");
+		}
+	}else{
+		printf("Error");
 	}
 }
